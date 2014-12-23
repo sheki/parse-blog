7a8379cd58f83b33e651513453a14126ebc46218
abhishekk <abhishek@parse.com>
2014-12-23 12:07:42 -0800

    actually deploy to parse

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -4,3 +4,5 @@ PARSE_CONFIG_DIR="parse_app/config"
 mkdir -p "$PARSE_CONFIG_DIR"
 cp -rf public parse_app/public
 go run scripts/gen_travis_parse_keys.go > "$PARSE_CONFIG_DIR/global.json"
+cd parse_app
+parse deploy
