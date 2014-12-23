4e00ccd25f37db9fb1785b8417c219010c414705
abhishekk <abhishek@parse.com>
2014-12-23 12:04:57 -0800

    build script generates parse app keys

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 $GOPATH/bin/hugo --theme=purehugo
-ls -l public
+PARSE_CONFIG_DIR="parse_app/config"
+mkdir -p "$PARSE_CONFIG_DIR"
+cp -rf public parse_app/public
+go run scripts/gen_travis_parse_keys.go > "$PARSE_CONFIG_DIR/global.json"
