b0a8e9e373a451bb07ecceece349a3b14386aad6
Abhishek Kona <akona@slack-corp.com>
2020-03-24 11:43:21 -0400

    Copy over workout website

---
+++ static/stretch/precache-manifest.35c6474646e2d28c6d29e741762053ef.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f285da0e4d390acd4192cd84f8b2694",
+    "url": "/strech/index.html"
+  },
+  {
+    "revision": "7b21736b060e20086d12",
+    "url": "/strech/static/css/main.11074b9c.chunk.css"
+  },
+  {
+    "revision": "1feb4ffb8a5b5f7ce631",
+    "url": "/strech/static/js/2.c07be269.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/strech/static/js/2.c07be269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b21736b060e20086d12",
+    "url": "/strech/static/js/main.1c9aa8a8.chunk.js"
+  },
+  {
+    "revision": "8211ab711129bd1f153c",
+    "url": "/strech/static/js/runtime-main.79b9319b.js"
+  },
+  {
+    "revision": "6fba3cbe0a1e13e96283ef9ff2e9271d",
+    "url": "/strech/static/media/butterfly.6fba3cbe.jpg"
+  },
+  {
+    "revision": "02be80cd7270d7b8783a2d2ed6e86df8",
+    "url": "/strech/static/media/fullsquat.02be80cd.jpg"
+  },
+  {
+    "revision": "34cd31c418fd77f9fb80c38f4c137b02",
+    "url": "/strech/static/media/hamstring.34cd31c4.webp"
+  },
+  {
+    "revision": "6456c4d89a11872ee3ee879e2d0266f6",
+    "url": "/strech/static/media/hellyeah.6456c4d8.webp"
+  },
+  {
+    "revision": "7387a2e118882857bb89732e79602613",
+    "url": "/strech/static/media/kneeleft.7387a2e1.jpg"
+  },
+  {
+    "revision": "40c52e9eedb29cf9aa9d7d97b1820c34",
+    "url": "/strech/static/media/kneeright.40c52e9e.jpg"
+  },
+  {
+    "revision": "a7608dafcf63756278b7166bfee873ea",
+    "url": "/strech/static/media/rearhandclasp.a7608daf.jpg"
+  },
+  {
+    "revision": "a732a0bc0cb7ee80d36b487d84db2d64",
+    "url": "/strech/static/media/shoulderStretch.a732a0bc.jpg"
+  },
+  {
+    "revision": "ab9fe8e030fd99c0eb2e4e6835486663",
+    "url": "/strech/static/media/underarmstertch.ab9fe8e0.jpg"
+  },
+  {
+    "revision": "2b43a39439f78446d0a8bf02cfa12547",
+    "url": "/strech/static/media/wheel.2b43a394.jpg"
+  }
+]);
