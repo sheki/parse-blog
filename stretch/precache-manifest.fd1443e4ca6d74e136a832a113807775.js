7c2469637fbcb33c0884f8923d0ca5e6fb36c264
sheki <sheki@users.noreply.github.com>
2025-01-27 21:35:27 +0000

    deploy: 3ad7699f6efc2bc2b25173a1aa4cb39c0cc60576

---
+++ stretch/precache-manifest.fd1443e4ca6d74e136a832a113807775.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e168c9c23e8906f944a23df0552f91",
+    "url": "/stretch/index.html"
+  },
+  {
+    "revision": "cc22a572eb344d691a4c",
+    "url": "/stretch/static/css/main.11074b9c.chunk.css"
+  },
+  {
+    "revision": "46d49178874689abd310",
+    "url": "/stretch/static/js/2.0b29da81.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/stretch/static/js/2.0b29da81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc22a572eb344d691a4c",
+    "url": "/stretch/static/js/main.2b3583db.chunk.js"
+  },
+  {
+    "revision": "7db895689f719bccccd0",
+    "url": "/stretch/static/js/runtime-main.cc33baa7.js"
+  },
+  {
+    "revision": "6fba3cbe0a1e13e96283ef9ff2e9271d",
+    "url": "/stretch/static/media/butterfly.6fba3cbe.jpg"
+  },
+  {
+    "revision": "02be80cd7270d7b8783a2d2ed6e86df8",
+    "url": "/stretch/static/media/fullsquat.02be80cd.jpg"
+  },
+  {
+    "revision": "34cd31c418fd77f9fb80c38f4c137b02",
+    "url": "/stretch/static/media/hamstring.34cd31c4.webp"
+  },
+  {
+    "revision": "6456c4d89a11872ee3ee879e2d0266f6",
+    "url": "/stretch/static/media/hellyeah.6456c4d8.webp"
+  },
+  {
+    "revision": "7387a2e118882857bb89732e79602613",
+    "url": "/stretch/static/media/kneeleft.7387a2e1.jpg"
+  },
+  {
+    "revision": "40c52e9eedb29cf9aa9d7d97b1820c34",
+    "url": "/stretch/static/media/kneeright.40c52e9e.jpg"
+  },
+  {
+    "revision": "a7608dafcf63756278b7166bfee873ea",
+    "url": "/stretch/static/media/rearhandclasp.a7608daf.jpg"
+  },
+  {
+    "revision": "a732a0bc0cb7ee80d36b487d84db2d64",
+    "url": "/stretch/static/media/shoulderStretch.a732a0bc.jpg"
+  },
+  {
+    "revision": "ab9fe8e030fd99c0eb2e4e6835486663",
+    "url": "/stretch/static/media/underarmstertch.ab9fe8e0.jpg"
+  },
+  {
+    "revision": "2b43a39439f78446d0a8bf02cfa12547",
+    "url": "/stretch/static/media/wheel.2b43a394.jpg"
+  }
+]);
