e6813473dc9f4b07f388239e6d24b6a88bad68d8
sheki <sheki@users.noreply.github.com>
2025-04-23 16:59:00 +0000

    deploy: 3a9118957c51172a172620b172ce5c8f7f4ad0ab

---
+++ js/vantara.js
@@ -0,0 +1,21 @@
+window._vntQueue = window._vntQueue || [];
+window.VNT = {
+  init: function () {
+    window._vntQueue.push(["init", Array.prototype.slice.call(arguments)]);
+  },
+  identify: function () {
+    window._vntQueue.push(["identify", Array.prototype.slice.call(arguments)]);
+  },
+};
+
+VNT.init(
+  "prd_00000000000000000000000001",
+  "key_s/OSk4v2rA72WtID+I9FEz5zITqaf4UUI7diJm1X0hA6WgQgf97crc1eoYZpvXHqpfhNU2IK6To/v7HUknAbXA==",
+  {
+    environment: "production",
+    version: "1.0.0", // optional
+    metadata: {
+      site: "sheki.in",
+    },
+  },
+);
