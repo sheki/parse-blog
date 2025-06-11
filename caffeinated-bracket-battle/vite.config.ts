14bbf756a2cfa4cdcc9a555c6f9f9026a94fc233
sheki <abhishek.kona@gmail.com>
2025-06-11 14:21:51 -0700

    Absolute paths

--- caffeinated-bracket-battle/vite.config.ts
+++ caffeinated-bracket-battle/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "",
+  base: "/coffee",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
