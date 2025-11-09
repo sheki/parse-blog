e97cb03618b1d9a0c2eb1b1135005c927348a2b9
sheki <abhishek.kona@gmail.com>
2025-11-09 15:03:54 -0800

    Journal app

--- apps/src/App.tsx
+++ apps/src/App.tsx
@@ -4,12 +4,14 @@ import HomePage from "./pages/Home";
 import CoffeeApp from "./pages/CoffeeApp";
 import BusApp from "./pages/BusApp";
 import StarRealmsApp from "./pages/StarRealmsApp";
+import JournalApp from "./pages/JournalApp";
 
 const App = () => (
   <TooltipProvider>
     <Router basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<JournalApp />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/coffee" element={<CoffeeApp />} />
         <Route path="/bus" element={<BusApp />} />
         <Route path="/star-realms" element={<StarRealmsApp />} />
